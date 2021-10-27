cd2362e9ba05967518a173f5f4a72075bd0f1e15
dreagonm <1748281467@qq.com>
2021-10-27 23:06:44 +0800

    [UPDATE] 配置文件独立

--- Connect.py
+++ Connect.py
@@ -2,16 +2,52 @@ import json
 import Spider
 import time
 import requests
+import os
 
-HOST = ''
-FILE_DIR = ''
-AuthKey = ''
-Session = None
-TimeStamp = ''
-CLEARTIME = 10
-Account = 0
-ReceiveCount = 10
-DELAYTIME = 2
+HOST = '' # BOT服务器地址
+FILE_DIR = '' # 缓存文件目录
+AuthKey = '' # AuthKey
+Session = None # Session（自动获取）
+CLEARTIME = 10 # Session释放计数
+Account = 0 # 账户
+ReceiveCount = 10 # 单次从队列读取的消息数
+DELAYTIME = 2 # 每次请求间隔
+
+# 默认config.json目录：../PyPluginConfig/AsoulFansCounting/Config.json
+# 或者 ./PyPluginConfig
+
+# ConfigPath = './PyPluginConfig'
+ConfigPath = '../PyPluginConfig/AsoulFansCounting/Config.json'
+ConfigFileName = '/Config.json'
+
+def GetConfig():
+    if os.path.isfile(ConfigPath+ConfigFileName):
+        global HOST,AuthKey,CLEARTIME,Account,ReceiveCount,DELAYTIME
+        with open(ConfigPath+ConfigFileName,'r') as f:
+            f.seek(0)
+            s = f.read()
+            data = json.loads(s)
+            HOST = data['HOST']
+            AuthKey = data['AuthKey']
+            CLEARTIME = data['CLEARTIME']
+            Account = data['Account']
+            ReceiveCount = data['ReceiveCount']
+            DELAYTIME = data['DELAYTIME']
+    else:
+        os.makedirs(ConfigPath,exist_ok=True)
+        with open(ConfigPath+ConfigFileName,'w') as f:
+            data = {
+                'HOST' : '',
+                'AuthKey' : '',
+                'CLEARTIME' : 10,
+                'Account' : 0,
+                'ReceiveCount' : 10,
+                'DELAYTIME' : 2
+            }
+            f.write(json.dumps(data))
+        print('请修改Config.json')
+        raise(Exception)
+
 
 def GetData():
     return Spider.run()
@@ -87,7 +123,7 @@ def Read():
     reda = json.loads(re.text)
     Qsz = reda["data"]
     if(Qsz > 0):
-        print("获取消息")
+        # print("获取消息")
         param = {
             'sessionKey' : Session,
             'count' : ReceiveCount
@@ -96,30 +132,31 @@ def Read():
         redata = json.loads(reMessage.text)
         Message = redata['data']
         for message in Message:
-            print("遍历")
+            # print("遍历")
             if message['type'] == "GroupMessage":
                 FlagCommand = False
                 GroupName = message['sender']['group']['id']
                 for item in message['messageChain']:
                     if(item['type']=='Plain' and item['text']=='今日a手'):
                         FlagCommand = True
-                        print("A手")
+                        # print("A手")
                 if(FlagCommand):
                     Send(GroupName)
 
 
 def run():
+    GetConfig()
     ReCnt = 0
     while(True):
         if(Session == None):
-            print("登录成功")
+            # print("登录成功")
             Auth()
         ReCnt = ReCnt + 1
-        print("第",ReCnt,"次读取")
+        # print("第",ReCnt,"次读取")
         Read()
         if(ReCnt == CLEARTIME):
             ReCnt = 0
-            print("释放成功")
+            # print("释放成功")
             Release()
         time.sleep(DELAYTIME)
 
