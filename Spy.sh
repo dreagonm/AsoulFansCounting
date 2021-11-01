04d2fd920b6e8fddead99f41a4cd1d54ca3177ef
dreagonm <1748281467@qq.com>
2021-11-01 13:18:05 +0800

    [UPDATE] 增加文字描述

--- Spy.sh
+++ Spy.sh
@@ -6,3 +6,4 @@ source {BOT_PATH}/PyPlugin/AsoulFansCounting/venv/bin/activate # 启用虚拟环
 {BOT_PATH}/PyPlugin/AsoulFansCounting/venv/bin/python3 {BOT_PATH}/PyPlugin/AsoulFansCounting/Spider.py #启动爬虫
 deactivate
 #使用时将{BOT_PATH}替换为机器人mcl.jar所在路径
+#按照自己的虚拟环境配置修改虚拟环境路径信息
