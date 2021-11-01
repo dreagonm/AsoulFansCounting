253416c20c0defaf44251f3d426dee501b051391
dreagonm <1748281467@qq.com>
2021-11-01 12:24:23 +0800

    [UPDATE] 添加Spy.sh

---
+++ Spy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin
+cd {BOT_PATH}/PyPlugin/AsoulFansCounting
+touch {BOT_PATH}/PyPlugin/AsoulFansCounting/run
+source {BOT_PATH}/PyPlugin/AsoulFansCounting/venv/bin/activate # 启用虚拟环境
+{BOT_PATH}/PyPlugin/AsoulFansCounting/venv/bin/python3 {BOT_PATH}/PyPlugin/AsoulFansCounting/Spider.py #启动爬虫
+deactivate
+#使用时将{BOT_PATH}替换为机器人mcl.jar所在路径
